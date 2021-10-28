4e733c5955d4793f3f5a92c9922a9f2fd3a469ea
Daniela Escobar <daniela.b.escobar@gmail.com>
2021-10-28 12:53:44 +0200

    Add project files.

---
+++ ListPhonebook/Person.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ListPhonebook
+{
+    class Person
+    {
+        public string FirstName { get; set; }
+        public string PhoneNumber { get; set; }
+
+        public static List<Person> Phonebook = new List<Person>();
+        public void AddPerson()
+        {
+            Person person = new();
+            Console.Write("Enter name: ");
+            person.FirstName = Console.ReadLine().ToUpper();
+            Console.Write("\nEnter phonenumber: ");
+            person.PhoneNumber = Console.ReadLine();
+
+            Phonebook.Add(person);
+        }
+
+        public static void PrintPhonebook(Person person)
+        {
+            Console.WriteLine("Name: " + person.FirstName);
+            Console.WriteLine("Phone Number: " + person.PhoneNumber);
+            Console.WriteLine("_____________________________________");
+        }
+
+        public void ListPeople()
+        {
+            if(Phonebook.Count == 0)
+            {
+                Console.WriteLine("There are no entries. . .");
+                Console.ReadKey();
+                return;
+            }
+            Console.WriteLine("Current entries:\n");
+            foreach (var person in Phonebook)
+            {
+                PrintPhonebook(person);
+            }
+            Console.WriteLine("\nPress any key to continue. . .");
+            Console.ReadKey();
+        }
+
+        public void RemovePerson()
+        {
+            Console.WriteLine("Which name do you want to remove?");
+            
+            foreach (var person in Phonebook)
+            {
+                PrintPhonebook(person);
+            }
+            Console.WriteLine("Enter a number (starting from 0 from the top) for the entry you want to remove");
+            int input = Convert.ToInt32(Console.ReadLine());
+            //Try-catch eller if-sats här för fel input
+            Phonebook.Remove(Phonebook[input]);
+            Console.WriteLine($"\nThe entry at index [{input}] has been removed.");
+            Console.ReadKey();
+
+        }
+    }
+}
---
+++ ListPhonebook/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace ListPhonebook
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            bool loop = true;
+            while (loop)
+            {
+                Console.Clear();
+                Person person = new();
+                Console.WriteLine("Choose option");
+                Console.WriteLine("1. Add new entry to phonebook 2. List entries in phonebook 3. Remove entry in phonebook");
+                Console.WriteLine("4. Exit");
+                ConsoleKey input = Console.ReadKey(true).Key;
+
+                switch (input)
+                {
+                    case ConsoleKey.D1:
+                        Console.Clear();
+                        person.AddPerson();    
+                        break;
+                    
+                    case ConsoleKey.D2:
+                        Console.Clear();
+                        person.ListPeople();
+                        break;
+                    
+                    case ConsoleKey.D3:
+                        Console.Clear();
+                        person.RemovePerson();
+                        break;
+                    
+                    case ConsoleKey.D4:
+                        Console.WriteLine("Shutting down. . .");
+                        Console.ReadLine();
+                        loop = false;
+                        break;
+                }
+            }
+        }
+    }
+}
